8694c5b5e0d47542f4a40ddf03c3f8178ca810fa
WuYaJun <yajun.wu@ucarinc.com>
2019-09-04 12:59:10 +0800

    修改pom, 不再使用外置tomcat启动

--- src/main/java/com/superme/demo/SuperApplication.java
+++ src/main/java/com/superme/demo/SuperApplication.java
@@ -2,16 +2,17 @@ package com.superme.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+/**
+ * description:<SpringBoot启动入口>
+ *
+ * @author 武亚军 (wuyajun0928@aliyun.com)
+ * @version 1.0
+ * @date 2019/8/25 11:50
+ */
 @SpringBootApplication
-public class SuperApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
-        return applicationBuilder.sources(SuperApplication.class);
-    }
+public class SuperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SuperApplication.class, args);
